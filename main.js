a2f73bff06f7e00be018831320ccde6160c950b8
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-07-17 01:50:56 +0800

    Simplify

--- main.js
+++ main.js
@@ -5,9 +5,8 @@
 // @match        https://pass.sdu.edu.cn/*
 // @grant        none
 // ==/UserScript==
-const YOUR_CUSTOM_DEVICE_FINGERPRINT = [
-  Object({ key: "My Browser", value: "Test code" }),
-];
+const YOUR_CUSTOM_DEVICE_FINGERPRINT = "SOMETHING_UNIQUE";
+
 (function () {
   "use strict";
 
@@ -39,70 +38,39 @@ const YOUR_CUSTOM_DEVICE_FINGERPRINT = [
     $("#ul").val(u.length);
     $("#pl").val(p.length);
     $("#rsa").val(strEnc(u + p + lt, "1", "2", "3"));
-
-    Fingerprint2.get(function (components) {
-      components = YOUR_CUSTOM_DEVICE_FINGERPRINT;
-      var details = "";
-      for (var index in components) {
-        var obj = components[index];
-        var line = obj.key + " = " + String(obj.value).substr(0, 100);
-        details += line + "\n";
-      }
-      var murmur = Fingerprint2.x64hash128(
-        components
-          .map(function (pair) {
-            return pair.value;
-          })
-          .join(),
-        31
-      );
-
-      var details_s = "";
-      for (var index in components) {
-        var obj = components[index];
-        if (
-          obj.key == "deviceMemory" ||
-          obj.key == "screenResolution" ||
-          obj.key == "availableScreenResolution"
-        ) {
-          continue;
+    var details_s = YOUR_CUSTOM_DEVICE_FINGERPRINT;
+    var murmur = YOUR_CUSTOM_DEVICE_FINGERPRINT;
+    var murmur_s = Fingerprint2.x64hash128(details_s, 31);
+    var murmur_md5 = hex_md5(details_s);
+    $.post(
+      "device",
+      {
+        d: murmur,
+        d_s: murmur_s,
+        d_md5: murmur_md5,
+        m: "1",
+        u: strEnc(u, "1", "2", "3"),
+        p: strEnc(p, "1", "2", "3"),
+      },
+      function (ret) {
+        if (ret.info == "validErr" || ret.info == "notFound") {
+          location.reload();
+        } else if (ret.info == "bind") {
+          //二次验证  绑定设备
+          $("#phone").val(ret.m);
+          phone(murmur_s, details_s);
+        } else if (ret.info == "mobileErr") {
+          //手机有误
+          $("#errormsg2").show().text("尚未绑定手机");
+        } else if (ret.info == "binded" || ret.info == "pass") {
+          //直接提交
+          $("#loginForm")[0].submit();
         }
-        var line = obj.key + " = " + String(obj.value).substr(0, 100);
-        details_s += line + "\n";
+      },
+      "json"
+    ).error(function (xhr, status, info) {
+      if (is_weixin()) {
       }
-      var murmur_s = Fingerprint2.x64hash128(details_s, 31);
-      var murmur_md5 = hex_md5(details_s);
-
-      $.post(
-        "device",
-        {
-          d: murmur,
-          d_s: murmur_s,
-          d_md5: murmur_md5,
-          m: "1",
-          u: strEnc(u, "1", "2", "3"),
-          p: strEnc(p, "1", "2", "3"),
-        },
-        function (ret) {
-          if (ret.info == "validErr" || ret.info == "notFound") {
-            location.reload();
-          } else if (ret.info == "bind") {
-            //二次验证  绑定设备
-            $("#phone").val(ret.m);
-            phone(murmur_s, details_s);
-          } else if (ret.info == "mobileErr") {
-            //手机有误
-            $("#errormsg2").show().text("尚未绑定手机");
-          } else if (ret.info == "binded" || ret.info == "pass") {
-            //直接提交
-            $("#loginForm")[0].submit();
-          }
-        },
-        "json"
-      ).error(function (xhr, status, info) {
-        if (is_weixin()) {
-        }
-      });
     });
   }
   // Override the original login function with the modified one
